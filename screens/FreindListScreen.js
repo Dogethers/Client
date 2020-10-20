e788d876f7df6f7d75c7c2f41b87274995741b98
syukurbaru <syukurbaru@gmail.com>
2020-10-20 16:37:12 +0700

    tamplate freindlist

---
+++ screens/FreindListScreen.js
@@ -0,0 +1,191 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  TextInput
+} from "react-native";
+import * as Animatable from "react-native-animatable";
+import { LinearGradient } from "expo-linear-gradient";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+
+const FreindListScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Friend Request</Text>
+
+        <ScrollView>
+          <View style={styles.action}>
+            <FontAwesome name="user" color="#05375a" size={20} />
+            <Text style={styles.textUser}>Syukur</Text>
+
+            <TouchableOpacity
+              onPress={() => navigation.navigate("WaitingRoom")}
+            >
+              <LinearGradient
+                colors={["#1F3C88", "#4f68ab"]}
+                style={[styles.room, { marginRight: 10 }]}
+              >
+                <Text style={[styles.textRoom, { color: "#fff" }]}>Accept</Text>
+              </LinearGradient>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("WaitingRoom")}
+            >
+              <LinearGradient
+                colors={["#EE6F57", "#ed5a3e"]}
+                style={styles.room}
+              >
+                <Text style={[styles.textRoom, { color: "#fff" }]}>Reject</Text>
+              </LinearGradient>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.action}>
+            <FontAwesome name="user" color="#05375a" size={20} />
+            <Text style={styles.textUser}>Vikri</Text>
+
+            <TouchableOpacity
+              onPress={() => navigation.navigate("WaitingRoom")}
+            >
+              <LinearGradient
+                colors={["#1F3C88", "#4f68ab"]}
+                style={[styles.room, { marginRight: 10 }]}
+              >
+                <Text style={[styles.textRoom, { color: "#fff" }]}>Accept</Text>
+              </LinearGradient>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("WaitingRoom")}
+            >
+              <LinearGradient
+                colors={["#EE6F57", "#ed5a3e"]}
+                style={styles.room}
+              >
+                <Text style={[styles.textRoom, { color: "#fff" }]}>Reject</Text>
+              </LinearGradient>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </View>
+
+      <Animatable.View style={styles.footer} animation="fadeInUpBig">
+        <Text style={styles.title}>Freind List</Text>
+
+        <ScrollView>
+
+        <View style={[styles.action,{marginBottom: 20}]}>
+            {/* <FontAwesome name="user" color="#05375a" size={20} /> */}
+            <TextInput
+                placeholder="Add your's freind"
+                placeholderTextColor="#666666" 
+                style={[styles.textUser,{backgroundColor: '#fff'}]} />
+            <TouchableOpacity
+              onPress={() => navigation.navigate("WaitingRoom")}
+            >
+              <LinearGradient
+                colors={["#1F3C88", "#4f68ab"]}
+                style={styles.room}
+              >
+                <Text style={[styles.textRoom, { color: "#fff" }]}>Add</Text>
+              </LinearGradient>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.action}>
+            <FontAwesome name="user" color="#05375a" size={20} />
+            <Text style={styles.textUser}>Bryan</Text>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("WaitingRoom")}
+            >
+              <LinearGradient
+                colors={["#EE6F57", "#ed5a3e"]}
+                style={styles.room}
+              >
+                <Text style={[styles.textRoom, { color: "#fff" }]}>Chat</Text>
+              </LinearGradient>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.action}>
+            <FontAwesome name="user" color="#05375a" size={20} />
+            <Text style={styles.textUser}>Bryan</Text>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("WaitingRoom")}
+            >
+              <LinearGradient
+                colors={["#EE6F57", "#ed5a3e"]}
+                style={styles.room}
+              >
+                <Text style={[styles.textRoom, { color: "#fff" }]}>Chat</Text>
+              </LinearGradient>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </Animatable.View>
+    </View>
+  );
+};
+
+export default FreindListScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  header: {
+    flex: 1,
+    // justifyContent: "center",
+    // alignItems: "center",
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+    backgroundColor: "#fff",
+  },
+  footer: {
+    flex: 1,
+
+    backgroundColor: "#E5E5E5",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+  title: {
+    color: "#05375a",
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  room: {
+    width: 70,
+    height: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  textRoom: {
+    fontSize: 16,
+    fontWeight: "bold",
+    padding: 5,
+  },
+  action: {
+    flexDirection: "row",
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#f2f2f2",
+    paddingBottom: 5,
+  },
+  textUser: {
+    flex: 1,
+    marginTop: Platform.OS === "ios" ? 0 : -2,
+    paddingLeft: 15,
+    color: "#05375a",
+    fontSize: 18,
+    marginRight: 10
+  },
+});
--- screens/HomeTabNavigator.js
+++ screens/HomeTabNavigator.js
@@ -2,7 +2,8 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './HomeScreen'
-import UsersScreen from './UserScreen'
+import FreindListScreen from './FreindListScreen'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 
 const Tab = createBottomTabNavigator();
@@ -10,9 +11,33 @@ const Tab = createBottomTabNavigator();
 export default function HomeTabNavigator() {
   return (
     
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Users" component={UsersScreen} />
+      <Tab.Navigator 
+        initialRouteName="Home"
+        tabBarOptions={{
+        activeTintColor: '#EE6F57',
+      }}
+      >
+        <Tab.Screen 
+          name="Home" 
+          component={HomeScreen}
+          options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }} 
+        />
+        
+        <Tab.Screen 
+          name="FreindListScreen" 
+          component={FreindListScreen} 
+          options={{
+          tabBarLabel: 'Freind',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+        />
       </Tab.Navigator>
    
   );
